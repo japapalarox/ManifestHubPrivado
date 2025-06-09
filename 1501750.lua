66abd11cc199cff2fd083c7d9727f4068e3db5dc
japapalarox <Japa852456@1>
2025-06-09 14:56:09 -0300

    Adicionando AppID 1501750 com manifest e chaves

---
+++ 1501750.lua
@@ -0,0 +1,17 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+addappid(1501750)
+addappid(1501751, 1, "c86a5d17ac060ed8c27bc9840a5eb75890d5d8cf1908993635a947d13a30076b")
+setManifestid(1501751, "7611787396434039283", 34943339340)
+addappid(2387920) -- Lords of the Fallen - Artbook (with depots)
+addappid(2387921, 1, "e69a9bfb62c1ca78c5fbb00518ff2251ada2f55dadfdbe8d842ee5174c83c3ac")
+setManifestid(2387921, "6553904194185226573", 185781686)
+
+-- DLCs without dedicated depots
+addappid(2370550) -- Lords of the Fallen - Legendary Pack
+addappid(2387870) -- Lords of the Fallen - Starting Boost
+addappid(2387880) -- Lords of the Fallen - Dark Crusader Starting Class
+addappid(2387890) -- Lords of the Fallen - Precious Metals Armour Tincts
+addappid(3563510) -- Lords of the Fallen - Full Entitlement
