7944061d7e937e304045a80b855b7780d2eb871f
japapalarox <Japa852456@1>
2025-06-10 09:45:16 -0300

    Adicionando AppID 2613950 com manifest e chaves

---
+++ 2613950.lua
@@ -0,0 +1,7 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the offical here: https://discord.gg/piracylords
+
+addappid(2613950)
+addappid(2613951,0,"298b10be2cce7ae9ad5e5acb64e48c100ab6be5af21ed54ffbd5210212f9c4da")
+setManifestid(2613951,"4818995176246964871")
