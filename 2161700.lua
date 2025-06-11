ff09a0d80d7ec92aa6299376ebc8af2b4b2b1047
japapalarox <Japa852456@1>
2025-06-11 09:04:29 -0300

    Adicionando AppID 2161700 com manifest e chaves

---
+++ 2161700.lua
@@ -0,0 +1,29 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+addappid(2161700)
+
+-- directx
+addappid(228988)
+setManifestid(228988,"6645201662696499616")
+
+-- directx
+addappid(228989)
+setManifestid(228989,"550968249685141759")
+
+-- directx
+addappid(228990)
+setManifestid(228990,"1829726630299308803")
+
+-- main app data
+addappid(2161701,0,"9644C57E48EB13144CD26FA9423B85051E1C474221F8F6CFCB017478CD8F3503")
+setManifestid(2161701,"4194654588156826962")
+
+-- dlc
+addappid(2517301,0,"99C413D3350860F2C095F2948891BDAF234AFF41E3DE3ECDCE62384D0DB89C1E")
+setManifestid(2517301,"3773053318858649709")
+
+-- dlc
+addappid(2517311,0,"6FE33390DB2C2102F8745EA2CFE432836E4337A33B4E78EF71456AA4E37529D0")
+setManifestid(2517311,"1834178036232257674")
