14736653ca72719b908b86fb740eefea368e35d2
japapalarox <Japa852456@1>
2025-06-12 09:02:53 -0300

    Adicionando AppID 1260320 com manifest e chaves

---
+++ 1260320.lua
@@ -0,0 +1,7 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+addappid(1260320)
+addappid(1260321, 1, "9623dafeee383701a837274d2547c7a5cbae7334393d64e82b4d2cdc01ef679e")
+setManifestid(1260321, "5531876536430070106", 0)
