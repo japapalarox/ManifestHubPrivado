a87545bb54d67741e160d88097ab07df32bc0a61
japapalarox <Japa852456@1>
2025-06-12 22:58:56 -0300

    Adicionando AppID 1416960 com manifest e chaves

---
+++ 1416960.lua
@@ -0,0 +1,18 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+addappid(1416960)
+addappid(1416961, 1, "2257e99decaf0453ec32df3a6762cc771e6ad768998a6dab444a236f02315163") 
+setManifestid(1416961, "6557051436687951906", 0)
+addappid(1416962, 1, "8054c729d9b4192f95afbfd8926be13d77400cc2444678b85ba5192ff3d34ab6") 
+setManifestid(1416962, "8862666444233950551", 0)
+addappid(1416963, 1, "c335a4cae8de8708503cee6322771bed0f459928d3a50333a3af3301fe90c76e")
+setManifestid(1416963, "7810741298026715717", 0)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") 
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") 
+setManifestid(228990, "1829726630299308803", 0)
+addappid(3024910)
+addappid(3024910, 1, "05aa9c5a57a13895ecc6ecdabb1854879c7a1c9de9089ed3247f37683c38e9be")
+setManifestid(3024910, "7798478129392709543", 0)
