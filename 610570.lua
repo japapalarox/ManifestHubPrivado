e9c28f3cb87383e6523127056f75f270617251e9
japapalarox <Japa852456@1>
2025-06-14 10:32:30 -0300

    Adicionando AppID 610570 com manifest e chaves

---
+++ 610570.lua
@@ -0,0 +1,7 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+addappid(610570)
+addappid(610571, 1, "e090cca9b8979c22db8ae6f778a5f9b44bf819fa259f3884ce6d37c3a6159d54")
+setManifestid(610571, "6986379750722896659", 0)
