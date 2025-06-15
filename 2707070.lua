a38356a71d5cfc022237d9c1eecb0375ec324aa2
japapalarox <Japa852456@1>
2025-06-14 21:33:11 -0300

    Adicionando AppID 2707070 com manifest e chaves

---
+++ 2707070.lua
@@ -0,0 +1,12 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+addappid(2707070)
+addappid(2707071, 1, "45615e6f882308fd298bcb21be7a3df4add896748f19c82ef96f52d48b95de36")
+setManifestid(2707071, "5351521945937704850", 0)
+addappid(2707072, 1, "97255bb5f8bd6943cb3b760c158efafb1f7a5f0042c0e15b49485bb128ab1b82")
+setManifestid(2707072, "6265205835501871040", 0)
+addappid(2707073, 1, "1440d88a0caea0b27f1dcb8159b3851d48b1adfb743b98de0c0f78c8034b4dfa")
+setManifestid(2707073, "5684618090523135989", 0)
+setManifestid(3405570, "7276380179457240299", 0)
