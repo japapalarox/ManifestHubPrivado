9e767da2df315ed942cdd916b539aa405e61225d
japapalarox <Japa852456@1>
2025-06-16 17:12:10 -0300

    Adicionando AppID 2215390 com manifest e chaves

--- 2215390.lua
+++ 2215390.lua
@@ -2,6 +2,6 @@
 -- Redistribution of VICTORs files is not allowed AT ALL
 -- Join the official here: https://discord.gg/piracylords
 
-addappid(342640)
-addappid(342641, 1, "63988ec4fd7cca0e3f903799219d0cde391a9b54f5f29d431a2c88a320fde776")
-setManifestid(342641, "7323174461956670721", 0)
+addappid(2215390)
+addappid(2215391,0,"d87a5534a048a1a7a81c896212cff7fa105e3104a74914bf1473552d72347211")
+setManifestid(2215391,"3833650524465557678")
