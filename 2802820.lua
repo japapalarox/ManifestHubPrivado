dfe2fd368f42acfbef9a553d9b56708ce01f9cf6
japapalarox <antunesjapapala@gmail.com>
2025-06-20 16:29:12 -0300

    Adicionando AppID 2802820 com manifest e chaves

---
+++ 2802820.lua
@@ -0,0 +1,11 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+addappid(2802820)
+addappid(2802821, 1, "b9b87b3927b1b8e5a42ff1c971d8e7da18f4471694f024fd2f9d55402345a76f")
+setManifestid(2802821, "3020736097987311147", 0)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
+setManifestid(228990, "1829726630299308803", 0)
