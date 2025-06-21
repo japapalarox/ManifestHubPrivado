757d9d38f2eebdca98a4b4cfc51ca90f5c236964
japapalarox <antunesjapapala@gmail.com>
2025-06-21 09:25:51 -0300

    Adicionando AppID 2622380 com manifest e chaves

--- 1812620.lua
+++
@@ -1,9 +0,0 @@
--- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
--- Redistribution of VICTORs files is not allowed AT ALL
--- Join the official here: https://discord.gg/piracylords
-
-addappid(1812620)
-addappid(229006)
-setManifestid(229006,"1784011429307107530")
-addappid(1812621,0,"b16191edf34d9d15597769ae5e091678e720af03f27763b92088c6eace30e1d4")
-setManifestid(1812621,"2023660974729136216")
---
+++ 2622380.lua
@@ -0,0 +1,22 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+addappid(2622380)
+addappid(2622381,0,"c3e9ccfedcda0cd10e53938e15261130b2e166007d2ca8f13aa5a05f06bd2630")
+setManifestid(2622381,"1414850870793307718")
+addappid(2622383,0,"c6e496ba5ae5c8bfa358f3a7d923ec39964fa0a41c4fa77006249fe34e677af2")
+setManifestid(2622383,"5185881824563081547")
+addappid(228988,0,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358")
+setManifestid(228988,"6645201662696499616")
+addappid(228990,0,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
+setManifestid(228990,"1829726630299308803")
+addappid(2622383,0,"c6e496ba5ae5c8bfa358f3a7d923ec39964fa0a41c4fa77006249fe34e677af2")
+setManifestid(2622383,"4206138204197642023")
+addappid(2622381,0,"c3e9ccfedcda0cd10e53938e15261130b2e166007d2ca8f13aa5a05f06bd2630")
+setManifestid(2622381,"8833519492745492170")
+addappid(2622384,0,"029f67382779483e0b34bc2f1a1fae303e7f83a4efead9ed076448f386afd28f")
+setManifestid(2622384,"8680058597361435870")
+addappid(3319490)
+addappid(3515610)
+addappid(3637850)
